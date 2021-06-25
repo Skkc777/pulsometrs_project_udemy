67ea6c6dd885203c3cef3925eed6a2aef4b78f47
Stanislav <skkc@yandex.ru>
2021-06-25 18:27:19 +0200

    pulsometr project

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+// Static server
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: ".",
+            index: "index.html"
+        }
+    });
+});
+
+gulp.task('styles', function () {
+    return gulp.src('scss/*.+(scss|sass)')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min"
+        }))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('scss/*.+(scss|sass)', gulp.parallel('styles'));
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
